79c64286913f26f9d2146cb270140318d88890b0
wocattree <695869983@qq.com>
2022-09-04 16:06:15 +0800

    建立服务端项目

---
+++ ThriftEnvironmentMonitor/EnvironmentMonitorServer/MonitorServer.cpp
@@ -0,0 +1,16 @@
+////////////////////////////////
+//filename:MonitorServer.cpp
+//description:实现服务器端的监控和调用
+//data:2022/09/04 13:35:55
+//author:wocattree
+////////////////////////////////
+
+#include <iostream>
+#include <chrono>
+#include <thread>
+
+
+int main()
+{
+	std::cout << "hello" << std::endl;
+}
